9a20ee006fd2ed64f55156517316f60799bb7fc9
uyentramle <uyentramle1010@gmail.com>
2024-10-14 14:57:56 +0700

    change default manager layout, add links sidebar

--- src/layouts/manager/Components/Sidebar/Sidebar.tsx
+++ src/layouts/manager/Components/Sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
-import { MenuOutlined, FormOutlined, SnippetsOutlined, } from '@ant-design/icons';
-import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
-import AnalyticsOutlinedIcon from '@mui/icons-material/AnalyticsOutlined';
+import {
+    MenuOutlined,
+    ProductOutlined,
+    DollarOutlined,
+    BarChartOutlined,
+    CaretRightOutlined,
+} from '@ant-design/icons';
 import { Menu, MenuProps } from 'antd';
 import Sider from 'antd/es/layout/Sider';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import img from '/assets/img/logo.png';
@@ -24,55 +28,50 @@ function getItem(
     } as MenuItem;
 }
 
-export default function MySider() {
+export default function MySider({
+    collapsed,
+    setCollapsed,
+}: {
+    collapsed: boolean;
+    setCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
     const navigate = useNavigate();
-
-    const [collapsed, setCollapsed] = useState(window.innerWidth < 1280);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setCollapsed(window.innerWidth < 1280);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
 
     const getConditionalItems = (): MenuItem[] => {
         return [
-            getItem('Bảng thống kê', '1', <AnalyticsOutlinedIcon />),
-            getItem('Quản lý bài viết', '2', <FormOutlined />),
-            getItem('Quản lý danh mục bài viết', '3', <SnippetsOutlined />),
-            getItem('Quản lý tài khoản', '4', <ManageAccountsOutlinedIcon />),
+            getItem('Bảng thống kê', '1', <BarChartOutlined />),
+            getItem('Quản lý gói dịch vụ', '2', <ProductOutlined />, [
+                getItem('Danh sách gói dịch vụ', '2-1', <CaretRightOutlined />),
+                getItem('Dịch vụ khách sử dụng', '2-2', <CaretRightOutlined />),
+            ]),
+            getItem('Quản lý thanh toán', '3', <DollarOutlined />),
         ];
     };
     const navUrl = new Map<string, string>();
     navUrl
-        .set('1', '#')
-        .set('2', '#')
-        .set('3', '#')
-        .set('4', '#');
+        .set('1', '/manager')
+        .set('2', '/manager/service-packages')
+        .set('2-1', '/manager/service-packages')
+        .set('2-2', '/manager/user-services')
+        .set('3', '#');
 
     return (
-        <>
+        <div className="fixed top-0 left-0 h-full bg-white z-50">
             <Sider
                 theme="light"
                 collapsible
                 collapsed={collapsed}
                 onCollapse={(value) => setCollapsed(value)}
-                className="overflow-hidden border-r-[1px] "
+                className="overflow-hidden border-r-[1px]"
                 trigger={
                     <div className="w-full border-r-[1px] border-t-[1px]">
-                        <MenuOutlined></MenuOutlined>
+                        <MenuOutlined />
                     </div>
                 }
                 width={256}
             >
                 <div className="demo-logo-vertical border-r-[1px] border-gray-200 pt-2 pb-4 flex justify-center">
-                    <a href='/'>
+                    <a href="/manager/">
                         <img src={img} alt="Logo" className="w-18" />
                     </a>
                 </div>
@@ -86,8 +85,8 @@ export default function MySider() {
                             navigate(link);
                         }
                     }}
-                ></Menu>
+                />
             </Sider>
-        </>
+        </div>
     );
 }
